3fe49c78ecd62e9d1a7ba08b02788cc07c3c0548
Jianzhuo Shen <2650826828@qq.com>
2019-05-15 12:32:52 +0000

    test 2-8

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -13,7 +13,7 @@
 
     var appUrl = process.env.APP_URL;
 
-    describe('Get Weather', function() {
+    describe('Get Weather(town name)', function() {
 
     	it('with valid town name', function(done) {
         if(!appUrl) {
@@ -61,6 +61,67 @@
         request({
       		method: 'GET',
               url: appUrl + '/api/v1/getWeather?zip=Auckland'
+          }, function(err, resp, body) {
+          	if(err) {
+          		assert.fail('Failed to get the response');
+          	} else {
+              assert.equal(resp.statusCode, 200);
+              var pbody = JSON.parse(body);
+              assert(pbody.city === 'Auckland', "City name does not match");
+              done();
+            }
+        });
+    	});
+    });
+    
+    describe('Get Weather2(town lat/lon)', function() {
+
+    	it('with valid town lat/lon', function(done) {
+        if(!appUrl) {
+            assert.fail("Environment variable APP_URL is not defined");
+            return done();
+        }
+        request({
+      		method: 'GET',
+              url: appUrl + '/api/v1/getWeather?lat=-41.29&lon=174.78'
+          }, function(err, resp, body) {
+          	if(err) {
+          		assert.fail('Failed to get the response');
+          	} else {
+              assert.equal(resp.statusCode, 200);
+              var pbody = JSON.parse(body);
+              assert((pbody.city === 'Wellington'), "City name does not match");
+              done();
+            }
+        });
+    	});
+
+      it('without lat/lon', function(done) {
+        if(!appUrl) {
+            assert.fail("Environment variable APP_URL is not defined");
+            return done();
+        }
+        request({
+      		method: 'GET',
+              url: appUrl + '/api/v1/getWeather'
+          }, /* @callback */ function(err, resp, body) {
+          	if(err) {
+          		assert.fail('Failed to get the response');
+          	} else {
+              assert.equal(resp.statusCode, 400);
+              done();
+            }
+        });
+    	});
+
+      it('with another valid lat/lon', function(done) {
+        if(!appUrl) {
+            assert.fail("Environment variable APP_URL is not defined");
+            return done();
+        }
+        request({
+      		method: 'GET',
+              url: appUrl + '/api/v1/getWeather?lat=-36.868372&lon=174.754743'
           }, function(err, resp, body) {
           	if(err) {
           		assert.fail('Failed to get the response');
