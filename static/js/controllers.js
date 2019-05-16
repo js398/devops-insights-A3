cc09f5751ea4ecf991621551fcdb25145883cba1
Jianzhuo Shen <2650826828@qq.com>
2019-05-16 21:04:43 +0000

    test 3-6

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -26,7 +26,7 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
     $scope.zip1City = "";
     $scope.zip1Weather = "";
 
-    $scope.zip = function(which) {
+    $scope.zip = function abc(which) {
 
         var data = "";
         if(which === 1) {
